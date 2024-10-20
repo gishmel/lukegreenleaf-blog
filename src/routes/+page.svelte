10a4aca289d4f04a95e907c155af1688dcc3b047
Luke Greenleaf <lukeagreenleaf@gmail.com>
2024-10-20 15:56:28 -0400

    Base for sveltekit github pages deployment.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { base } from "$app/paths";
+</script>
+
+<h1>Luke Greenleaf's Technical Blog:</h1>
+<p>Deployed to GitHub Pages.</p>
+<a href="{base}/greenleafgames">Greenleaf Games</a>
